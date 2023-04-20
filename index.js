3b180216c146889a1f008602807568c47a0780c9
na-balazs <72384610+na-balazs@users.noreply.github.com>
2023-04-20 15:45:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import {
+  todoItem,
+  disableAddButton,
+  enableAddButton,
+  removeForm,
+  addListItem,
+  globalInfo,
+  createList,
+} from "./items.js";
+
+enableAddButton();
+
+console.log(globalInfo);
